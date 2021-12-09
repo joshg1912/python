5a2b7528cafe7409b10069457d2ce16d72fba7ea
Josh <joshgarcia7410@gmail.com>
2021-12-08 18:57:47 -0800

    changes health cap

--- health_potion.py
+++ health_potion.py
@@ -4,7 +4,7 @@ health = 50
 
 health_potion = random.randint(10,20)
 
-if health + health_potion > 65:
+if health + health_potion >= 70:
     print("health capped!")
 else:
       print(health + health_potion)
